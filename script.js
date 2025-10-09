58dbdb069a6b00e101461a55f069204315a4c854
lmartinez20117 <lmartinez20117@iesjoanramis.org>
2025-10-09 10:45:11 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 function exercici1(){
-   var nom = "Lola Martínez Apellániz" 
-   document.writeln(nom)
-  
+   document.writeln("Lola Martínez")
+   document.writeln("1r Batxillerat")
+   document.writeln("Científic")
+   document.writeln("2025")
+   document.writeln("Carrera universitària de medicina")
 }
+function exercici2(){
+   let nom = "Lola Martínez"
+   document.writeln("Lola Martínez")
+   let curs = "1r Batx"
+   document.writeln("1r Batxillerat")
+   let bachi = "Científic"
+   document.writeln("Científic")
+   let any = "año"
+   document.writeln("2025")
+   let estudis = "medicina"
+   document.writeln("Carrera universitària de medicina")
+}
+function exercici3(){
+   let nom = "Lola"
+   let cognom = "Martínez Apellániz"
+   let edat = "16"
+   let peli = "A través de mi ventana"
+   let any = "2022"
+   document.writeln("Hola em dic "+nom+" i el meu cognom és "+cognom+", tenc "+edat+" i la meva pel·lícula preferida és "+peli+" del "+any)
+}
+
+function exercici4(){
+   let notes = 5+6+8+2+7
+   let mitjana = notes/5
+   document.writeln("La suma de les notes es "+notes+ " i la seva mitjana es: "+mitjana)
+}
+
+
+
